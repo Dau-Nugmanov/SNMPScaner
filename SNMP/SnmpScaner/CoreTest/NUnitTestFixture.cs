0da2821ae08fc0b8358d670b564242daf4eb5a94
Dau-Nugmanov <Dau-Nugmanov@mail.ru>
2014-04-24 23:28:40 +0400

    Добавил полезные зависимости

---
+++ SNMP/SnmpScaner/CoreTest/NUnitTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core;
+using NUnit.Framework;
+
+namespace CoreTest
+{
+	[TestFixture]
+	public class CoreTest
+	{
+		[Test]
+		public void Test()
+		{
+			SnmpScanServer t = new SnmpScanServer();
+			t.Run();
+		}
+	}
+}
+
