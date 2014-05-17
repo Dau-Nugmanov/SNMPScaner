6aef0ba402c17699dcb35fd53d1c8a18aa2816a2
Alexander Borozdyuhin <alborozd@gmail.com>
2014-05-17 18:40:58 +0400

    Оповещение на страницу + аварийные устройства

---
+++ SNMP/SnmpScaner/UI/Code/WarningsStorage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UI.Helpers;
+using UI.Models;
+
+namespace UI.Code
+{
+    public static class WarningsStorage
+    {
+        private static SortedList<int, WarningModel> _warningNotifications = new SortedList<int, WarningModel>();
+        private static int _maxWarningsCount = AppSettingsHelper.GetMaxWarningsCount;
+        
+        public static void AddWarning(WarningModel warning)
+        {
+            if (_warningNotifications.Count >= _maxWarningsCount)
+                _warningNotifications.Clear();
+            _warningNotifications.Add(_warningNotifications.Count, warning);
+        }
+
+        public static IEnumerable<WarningModel> GetWarnings()
+        {
+            return _warningNotifications.Values.AsEnumerable<WarningModel>();
+        }
+
+        public static int GetCount()
+        {
+            return _warningNotifications.Count;
+        }
+    }
+}
---
+++ SNMP/SnmpScaner/UI/Controllers/WarningsController.cs
@@ -0,0 +1,46 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UI.Code;
+
+namespace UI.Controllers
+{
+    public class WarningsController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetLastTimeStamp()
+        {
+            return Json(WarningsStorage.GetCount());
+        }
+
+        [HttpPost]
+        public JsonResult IsHavNewWarnings(int? currentNumber)
+        {
+            int curCount = WarningsStorage.GetCount();
+            if ((currentNumber.HasValue && curCount > currentNumber)
+                || (!currentNumber.HasValue && curCount > 0))
+                return Json(new {state = true, count = curCount});
+            return Json(false);
+        }
+
+        public ActionResult GetAll()
+        {
+            return View(WarningsStorage.GetWarnings());
+        }
+
+        public ActionResult Details(int id)
+        {
+            var devicesRepo = new DevicesRepository(new SnmpDbContext());
+            var device = devicesRepo.GetById(id);
+            return View(device);
+        }
+    }
+}
